57d58197c56562dfd4f2a3f39fe234becb6fe266
Endri Kacupaj <endrikacupaj@gmail.com>
2020-08-04 15:31:56 +0200

    - add entity type on elasticse

--- scripts/csqa_elasticse.py
+++ scripts/csqa_elasticse.py
@@ -3,19 +3,20 @@ from unidecode import unidecode
 from elasticsearch import Elasticsearch
 from knowledge_graph.knowledge_graph import KnowledgeGraph
 
-
-entities = list(KnowledgeGraph().id_entity.items())
-print(f'Num of wikidata entities: {len(entities)}')
+kg = KnowledgeGraph()
+kg_entities = list(kg.id_entity.items())
+kg_types = kg.entity_type
+print(f'Num of wikidata entities: {len(kg_entities)}')
 
 es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 # es.indices.delete(index='csqa_wikidata', ignore=[400, 404])
 
 tic = time.perf_counter()
-for i, ent in enumerate(entities):
-    es.index(index='csqa_wikidata', doc_type='entities', id=i+1, body={'id': ent[0], 'label': unidecode(ent[1])})
-    if (i+1) % 10000 == 0: print(f'==> Finished {((i+1)/len(entities))*100:.4f}% -- {time.perf_counter() - tic:0.2f}s')
+for i, (id, label) in enumerate(kg_entities):
+    es.index(index='csqa_wikidata', doc_type='entities', id=i+1, body={'id': id, 'label': unidecode(label), 'type': kg_types[id] if id in kg_types else []})
+    if (i+1) % 10000 == 0: print(f'==> Finished {((i+1)/len(kg_entities))*100:.4f}% -- {time.perf_counter() - tic:0.2f}s')
 
-# query = unidecode('Đồng Gia')
+# query = unidecode('Abdul Qayyum Khan')
 # res = es.search(index='csqa_wikidata', doc_type='entities', body={
 #         'query': {
 #             'match': {
