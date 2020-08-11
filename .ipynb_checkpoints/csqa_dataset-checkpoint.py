3cf5dab1956ea273aca13d383a2c9ade23db41df
joanPlepi <joanplepi@gmail.com>
2020-08-11 18:22:17 +0200

    gnn current state

---
+++ .ipynb_checkpoints/csqa_dataset-checkpoint.py
@@ -0,0 +1,330 @@
+"""CSQADataset"""
+import os
+import re
+import json
+import logging
+import torch
+import numpy as np
+from glob import glob
+from pathlib import Path
+from transformers import BertTokenizer
+from torchtext.data import Field, Example, Dataset
+from const import (INPUT, LOGICAL_FORM, NER, COREF, START_TOKEN, END_TOKEN,
+                    CTX_TOKEN, PAD_TOKEN, UNK_TOKEN, SEP_TOKEN, PREDICATE, TYPE)
+
+class CSQADataset(object):
+    """CSQADataset class"""
+    TOKENIZE_SEQ = lambda self, x: x.split()
+    ROOT_PATH = Path(os.path.dirname(__file__))
+
+    def __init__(self, data_dir='/data/final'):
+        self.train_path = str(self.ROOT_PATH) + data_dir + '/train/*'
+        self.val_path = str(self.ROOT_PATH) + data_dir + '/val/*'
+        self.test_path = str(self.ROOT_PATH) + data_dir + '/test/*'
+        self.load_data_and_fields()
+
+    def _prepare_data(self, data):
+        input_data = []
+        for conversation in data:
+            prev_user = None
+            prev_system = None
+            is_clarification = False
+            turns = len(conversation) // 2
+            for i in range(turns):
+                input = []
+                logical_form = []
+                ner_tag = []
+                coref = []
+                coref_gold_idx = []
+                predicate_cls = []
+                type_cls = []
+
+                if is_clarification:
+                    is_clarification = False
+                    continue
+
+                user = conversation[2*i]
+                system = conversation[2*i + 1]
+
+                # skip if example is spurious
+                if user['question-type'] != 'Clarification':
+                    if user['is_ner_spurious'] or system['is_ner_spurious'] or system['is_spurious']:
+                        continue
+
+                # skip if previous example was spurious
+                if i > 0 and prev_user_conv['question-type'] != 'Clarification':
+                    if prev_user_conv['is_ner_spurious'] or prev_system_conv['is_ner_spurious'] or prev_system_conv['is_spurious']:
+                        continue
+                
+                #TODO: Remove NA tokens.
+                if i == 0: # NA + [SEP] + NA + [SEP] + current_question
+                    input.extend([  'NA', SEP_TOKEN, 'NA', SEP_TOKEN])
+                    ner_tag.extend(['O',  'O',       'O',  'O'])
+                else:
+                    # add prev context user
+                    for context in prev_user_conv['context']:
+                        input.append(context[1])
+                        ner_tag.append(f'{context[-1]}')
+
+                    # sep token
+                    input.append(SEP_TOKEN)
+                    ner_tag.append('O')
+
+                    # add prev context answer
+                    for context in prev_system_conv['context']:
+                        input.append(context[1])
+                        ner_tag.append(f'{context[-1]}')
+
+                    # sep token
+                    input.append(SEP_TOKEN)
+                    ner_tag.append('O')
+
+                if user['question-type'] == 'Clarification':
+                    # get next context
+                    is_clarification = True
+                    next_user = conversation[2*(i+1)]
+                    next_system = conversation[2*(i+1) + 1]
+
+                    # user context
+                    for context in user['context']:
+                        input.append(context[1])
+                        ner_tag.append(f'{context[-1]}')
+
+                    # system context
+                    for context in system['context']:
+                        input.append(context[1])
+                        ner_tag.append(f'{context[-1]}')
+
+                    # next user context
+                    for context in next_user['context']:
+                        input.append(context[1])
+                        ner_tag.append(f'{context[-1]}')
+
+                    if 'gold_actions' not in next_system:
+                        continue
+
+                    # coref entities - prepare coref values
+                    coref_ent = set([action[1] for action in next_system['gold_actions'] if action[0] == 'entity'])
+                    for context in reversed(user['context'] + system['context'] + next_user['context']):
+                        if context[2] in coref_ent and context[4] == 'B':
+                            coref.append('1')
+                            coref_ent.remove(context[2])
+                        else:
+                            coref.append('0')
+
+                    if i == 0:
+                        coref.extend(['0', '0', '0', '0'])
+                    if i > 0:
+                        coref.append('0')
+                        for context in reversed(prev_system_conv['context']):
+                            if context[2] in coref_ent and context[4] == 'B':
+                                coref.append('1')
+                                coref_ent.remove(context[2])
+                            else:
+                                coref.append('0')
+                        coref.append('0')
+                        for context in reversed(prev_user_conv['context']):
+                            if context[2] in coref_ent and context[4] == 'B':
+                                coref.append('1')
+                                coref_ent.remove(context[2])
+                            else:
+                                coref.append('0')
+
+                    # get gold actions
+                    gold_actions = next_system['gold_actions']
+
+                    # track context history
+                    prev_user_conv = next_user.copy()
+                    prev_system_conv = next_system.copy()
+                else:
+                    # user context
+                    for context in user['context']:
+                        input.append(context[1])
+                        ner_tag.append(f'{context[-1]}')
+
+                    if 'gold_actions' not in system:
+                        continue
+
+                    # coref entities - prepare coref values
+                    coref_ent = set([action[1] for action in system['gold_actions'] if action[0] == 'entity'])
+                    for context in reversed(user['context']):
+                        if context[2] in coref_ent and context[4] == 'B':
+                            coref.append('1')
+                            coref_ent.remove(context[2])
+                        else:
+                            coref.append('0')
+
+                    if i == 0:
+                        coref.extend(['0', '0', '0', '0'])
+                    if i > 0:
+                        coref.append('0')
+                        for context in reversed(prev_system_conv['context']):
+                            if context[2] in coref_ent and context[4] == 'B':
+                                coref.append('1')
+                                coref_ent.remove(context[2])
+                            else:
+                                coref.append('0')
+                        coref.append('0')
+                        for context in reversed(prev_user_conv['context']):
+                            if context[2] in coref_ent and context[4] == 'B':
+                                coref.append('1')
+                                coref_ent.remove(context[2])
+                            else:
+                                coref.append('0')
+
+                    # get gold actions
+                    gold_actions = system['gold_actions']
+
+                    # track context history
+                    prev_user_conv = user.copy()
+                    prev_system_conv = system.copy()
+
+                # prepare logical form
+                for action in gold_actions:
+                    if action[0] == 'action':
+                        logical_form.append(action[1])
+                        predicate_cls.append('NA')
+                        type_cls.append('NA')
+                    elif action[0] == 'relation':
+                        logical_form.append('relation')
+                        predicate_cls.append(action[1])
+                        type_cls.append('NA')
+                    elif action[0] == 'type':
+                        logical_form.append('type')
+                        predicate_cls.append('NA')
+                        type_cls.append(action[1])
+                    elif action[0] == 'entity':
+                        if action[1] == 'prev_answer':
+                            logical_form.append('prev_answer')
+                        else:
+                            logical_form.append('entity')
+                        predicate_cls.append('NA')
+                        type_cls.append('NA')
+                    elif action[0] == 'value':
+                        logical_form.append(action[0])
+                        predicate_cls.append('NA')
+                        type_cls.append('NA')
+                    else:
+                        raise Exception(f'Unkown logical form {action[0]}')
+
+                assert len(input) == len(ner_tag)
+                assert len(input) == len(coref)
+                assert len(logical_form) == len(predicate_cls)
+                assert len(logical_form) == len(type_cls)
+
+                input_data.append([input, logical_form, ner_tag, list(reversed(coref)), predicate_cls, type_cls])
+
+        return input_data
+
+    def _make_torchtext_dataset(self, data, fields):
+        examples = [Example.fromlist(i, fields) for i in data]
+        return Dataset(examples, fields)
+
+    def load_data_and_fields(self):
+        """
+        Load data
+        Create source and target fields
+        """
+        train, val, test = [], [], []
+        # read data
+        train_files = glob(self.train_path + '/*.json')
+        for f in train_files:
+            with open(f) as json_file:
+                train.append(json.load(json_file))
+
+        val_files = glob(self.val_path + '/*.json')
+        for f in val_files:
+            with open(f) as json_file:
+                val.append(json.load(json_file))
+
+        test_files = glob(self.test_path + '/*.json')
+        for f in test_files:
+            with open(f) as json_file:
+                test.append(json.load(json_file))
+
+        # prepare data
+        train = self._prepare_data(train)
+        val = self._prepare_data(val)
+        test = self._prepare_data(test)
+
+        # create fields
+        self.input_field = Field(init_token=START_TOKEN,
+                                eos_token=CTX_TOKEN,
+                                pad_token=PAD_TOKEN,
+                                unk_token=UNK_TOKEN,
+                                lower=True,
+                                batch_first=True)
+
+        self.lf_field = Field(init_token=START_TOKEN,
+                                eos_token=END_TOKEN,
+                                pad_token=PAD_TOKEN,
+                                unk_token=UNK_TOKEN,
+                                lower=True,
+                                batch_first=True)
+
+        self.ner_field = Field(init_token='O',
+                                eos_token='O',
+                                pad_token='O',
+                                unk_token='O',
+                                batch_first=True)
+
+        self.coref_field = Field(init_token='0',
+                                eos_token='0',
+                                pad_token='0',
+                                unk_token='0',
+                                batch_first=True)
+
+        self.predicate_field = Field(init_token='NA',
+                                eos_token='NA',
+                                pad_token='NA',
+                                unk_token='NA',
+                                batch_first=True)
+
+        self.type_field = Field(init_token='NA',
+                                eos_token='NA',
+                                pad_token='NA',
+                                unk_token='NA',
+                                batch_first=True)
+
+        fields_tuple = [(INPUT, self.input_field), (LOGICAL_FORM, self.lf_field),
+                        (NER, self.ner_field), (COREF, self.coref_field),
+                        (PREDICATE, self.predicate_field), (TYPE, self.type_field)]
+
+        # create toechtext datasets
+        self.train_data = self._make_torchtext_dataset(train, fields_tuple)
+        self.val_data = self._make_torchtext_dataset(val, fields_tuple)
+        self.test_data = self._make_torchtext_dataset(test, fields_tuple)
+
+        # build vocabularies
+        self.input_field.build_vocab(self.train_data, self.val_data, self.test_data, min_freq=0, vectors='glove.840B.300d')
+        self.lf_field.build_vocab(self.train_data, min_freq=0)
+        self.ner_field.build_vocab(self.train_data, min_freq=0)
+        self.coref_field.build_vocab(self.train_data, min_freq=0)
+        self.predicate_field.build_vocab(self.train_data, self.val_data, self.test_data, min_freq=0)
+        self.type_field.build_vocab(self.train_data, self.val_data, self.test_data, min_freq=0)
+
+    def get_data(self):
+        """Return train, validation and test data objects"""
+        return self.train_data, self.val_data, self.test_data
+
+    def get_fields(self):
+        """Return source and target field objects"""
+        return {
+            INPUT: self.input_field,
+            LOGICAL_FORM: self.lf_field,
+            NER: self.ner_field,
+            COREF: self.coref_field,
+            PREDICATE: self.predicate_field,
+            TYPE: self.type_field
+        }
+
+    def get_vocabs(self):
+        """Return source and target vocabularies"""
+        return {
+            INPUT: self.input_field.vocab,
+            LOGICAL_FORM: self.lf_field.vocab,
+            NER: self.ner_field.vocab,
+            COREF: self.coref_field.vocab,
+            PREDICATE: self.predicate_field.vocab,
+            TYPE: self.type_field.vocab
+        }
---
+++ .ipynb_checkpoints/train-checkpoint.py
@@ -0,0 +1,213 @@
+import os
+import sys
+import time
+import random
+import logging
+import torch
+import numpy as np
+import torch.optim
+import torch.nn as nn
+from pathlib import Path
+from args import get_parser
+from models.model import ConvQA
+from csqa_dataset import CSQADataset
+from torchtext.data import BucketIterator
+from utils import NoamOpt, AverageMeter, save_checkpoint, init_weights
+from utils import (INPUT, LOGICAL_FORM, NER, COREF, PAD_TOKEN, PREDICATE, TYPE)
+from utils import NerLoss, CorefLoss, LogicalFormLoss, MultiTaskLoss
+
+# set root path
+ROOT_PATH = Path(os.path.dirname(__file__))
+
+# read parser
+parser = get_parser()
+args = parser.parse_args()
+
+# set logger
+logging.basicConfig(format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    datefmt='%d/%m/%Y %I:%M:%S %p',
+                    level=logging.INFO,
+                    handlers=[
+                        logging.FileHandler(f'{args.path_results}/train.log', 'w'),
+                        logging.StreamHandler()
+                    ])
+logger = logging.getLogger(__name__)
+
+# set a seed value
+random.seed(args.seed)
+np.random.seed(args.seed)
+if torch.cuda.is_available():
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed(args.seed)
+    torch.cuda.manual_seed_all(args.seed)
+
+# define device
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+torch.cuda.set_device(1)
+
+def main():
+    # load data
+    dataset = CSQADataset(args.data_path)
+    vocabs = dataset.get_vocabs()
+    train_data, val_data, _ = dataset.get_data()
+
+    # load model
+    model = ConvQA(vocabs).to(DEVICE)
+
+    # initialize model weights
+    init_weights(model)
+
+    logger.info(f'The model has {sum(p.numel() for p in model.parameters() if p.requires_grad):,} trainable parameters')
+
+    # define loss function (criterion)
+    criterion = {
+        'ner': NerLoss,
+        'coref': CorefLoss,
+        'logical_form': LogicalFormLoss,
+        'predicate': LogicalFormLoss,
+        'type': LogicalFormLoss,
+        'multi_task': MultiTaskLoss
+    }[args.task](ignore_index=vocabs[LOGICAL_FORM].stoi[PAD_TOKEN])
+
+    # define optimizer
+    optimizer = NoamOpt(torch.optim.Adam(model.parameters(), lr=0, betas=(0.9, 0.98), eps=1e-9))
+
+    if args.resume:
+        if os.path.isfile(args.resume):
+            logger.info(f"=> loading checkpoint '{args.resume}''")
+            checkpoint = torch.load(args.resume)
+            args.start_epoch = checkpoint['epoch']
+            best_val = checkpoint['best_val']
+            model.load_state_dict(checkpoint['state_dict'])
+            optimizer.optimizer.load_state_dict(checkpoint['optimizer'])
+            logger.info(f"=> loaded checkpoint '{args.resume}' (epoch {checkpoint['epoch']})")
+        else:
+            logger.info(f"=> no checkpoint found at '{args.resume}'")
+            best_val = float('inf')
+    else:
+        best_val = float('inf')
+
+    # prepare training and validation loader
+    train_loader, val_loader = BucketIterator.splits((train_data, val_data),
+                                                    batch_size=args.batch_size,
+                                                    sort_within_batch=False,
+                                                    sort_key=lambda x: len(x.input),
+                                                    device=DEVICE)
+
+    logger.info('Loaders prepared.')
+    logger.info(f"Training data: {len(train_data.examples)}")
+    logger.info(f"Validation data: {len(val_data.examples)}")
+    logger.info(f'Question example: {train_data.examples[0].input}')
+    logger.info(f'Logical form example: {train_data.examples[0].logical_form}')
+    logger.info(f"Unique tokens in input vocabulary: {len(vocabs[INPUT])}")
+    logger.info(f"Unique tokens in logical form vocabulary: {len(vocabs[LOGICAL_FORM])}")
+    logger.info(f"Unique tokens in ner vocabulary: {len(vocabs[NER])}")
+    logger.info(f"Unique tokens in coref vocabulary: {len(vocabs[COREF])}")
+    logger.info(f"Unique tokens in predicate vocabulary: {len(vocabs[PREDICATE])}")
+    logger.info(f"Unique tokens in type vocabulary: {len(vocabs[TYPE])}")
+    logger.info(f'Batch: {args.batch_size}')
+    logger.info(f'Epochs: {args.epochs}')
+
+    # run epochs
+    for epoch in range(args.start_epoch, args.epochs):
+        # train for one epoch
+        train(train_loader, model, vocabs, criterion, optimizer, epoch)
+
+        # evaluate on validation set
+        if (epoch+1) % args.valfreq == 0:
+            val_loss = validate(val_loader, model, vocabs, criterion)
+            if val_loss < best_val:
+                best_val = min(val_loss, best_val)
+                save_checkpoint({
+                    'epoch': epoch + 1,
+                    'state_dict': model.state_dict(),
+                    'best_val': best_val,
+                    'optimizer': optimizer.optimizer.state_dict(),
+                    'curr_val': val_loss})
+            logger.info(f'* Val loss: {val_loss:.4f}')
+
+def train(train_loader, model, vocabs, criterion, optimizer, epoch):
+    batch_time = AverageMeter()
+    losses = AverageMeter()
+
+    # switch to train mode
+    model.train()
+
+    end = time.time()
+    for i, batch in enumerate(train_loader):
+        # get inputs
+        input = batch.input
+        logical_form = batch.logical_form
+        ner = batch.ner
+        coref = batch.coref
+        predicate_cls = batch.predicate
+        type_cls = batch.type
+
+        # compute output
+        output = model(input, logical_form[:, :-1])
+
+        # prepare targets
+        target = {
+            'ner': ner.contiguous().view(-1),
+            'coref': coref.contiguous().view(-1),
+            'logical_form': logical_form[:, 1:].contiguous().view(-1), # (batch_size * trg_len)
+            'predicate': predicate_cls[:, 1:].contiguous().view(-1),
+            'type': type_cls[:, 1:].contiguous().view(-1),
+        }
+
+        # compute loss
+        loss = criterion(output, target)
+
+        # record loss
+        losses.update(loss.data, input.size(0))
+
+        # compute gradient and do Adam step
+        optimizer.zero_grad()
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), args.clip)
+        optimizer.step()
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        logger.info(f'Epoch: {epoch+1} - Train loss: {losses.val:.4f} ({losses.avg:.4f}) - Batch: {((i+1)/len(train_loader))*100:.2f}% - Time: {batch_time.sum:0.2f}s')
+
+def validate(val_loader, model, vocabs, criterion):
+    losses = AverageMeter()
+
+    # switch to evaluate mode
+    model.eval()
+
+    with torch.no_grad():
+        for _, batch in enumerate(val_loader):
+            # get inputs
+            input = batch.input
+            logical_form = batch.logical_form
+            ner = batch.ner
+            coref = batch.coref
+            predicate_cls = batch.predicate
+            type_cls = batch.type
+
+            # compute output
+            output = model(input, logical_form[:, :-1])
+
+            # prepare targets
+            target = {
+                'ner': ner.contiguous().view(-1),
+                'coref': coref.contiguous().view(-1),
+                'logical_form': logical_form[:, 1:].contiguous().view(-1), # (batch_size * trg_len)
+                'predicate': predicate_cls[:, 1:].contiguous().view(-1),
+                'type': type_cls[:, 1:].contiguous().view(-1),
+            }
+
+            # compute loss
+            loss = criterion(output, target)
+
+            # record loss
+            losses.update(loss.data, input.size(0))
+
+    return losses.avg
+
+if __name__ == '__main__':
+    main()
