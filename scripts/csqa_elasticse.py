a201418f399be34cb4c4ed5cf3dc0c09fc29f33f
Endri Kacupaj <endrikacupaj@gmail.com>
2020-08-28 12:39:59 +0200

    - add scripts for elasticse and dataset stats

--- scripts/csqa_elasticse.py
+++ scripts/csqa_elasticse.py
@@ -1,22 +1,22 @@
 import time
 from unidecode import unidecode
 from elasticsearch import Elasticsearch
-# from knowledge_graph.knowledge_graph import KnowledgeGraph
+from knowledge_graph.knowledge_graph import KnowledgeGraph
 
-# kg = KnowledgeGraph()
-# kg_entities = list(kg.id_entity.items())
-# kg_types = kg.entity_type
-# print(f'Num of wikidata entities: {len(kg_entities)}')
+kg = KnowledgeGraph()
+kg_entities = list(kg.id_entity.items())
+kg_types = kg.entity_type
+print(f'Num of wikidata entities: {len(kg_entities)}')
 
 es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 # es.indices.delete(index='csqa_wikidata', ignore=[400, 404])
 
-# tic = time.perf_counter()
-# for i, (id, label) in enumerate(kg_entities):
-#     es.index(index='csqa_wikidata', doc_type='entities', id=i+1, body={'id': id, 'label': unidecode(label), 'type': kg_types[id] if id in kg_types else []})
-#     if (i+1) % 10000 == 0: print(f'==> Finished {((i+1)/len(kg_entities))*100:.4f}% -- {time.perf_counter() - tic:0.2f}s')
+tic = time.perf_counter()
+for i, (id, label) in enumerate(kg_entities):
+    es.index(index='csqa_wikidata', doc_type='entities', id=i+1, body={'id': id, 'label': unidecode(label), 'type': kg_types[id] if id in kg_types else []})
+    if (i+1) % 10000 == 0: print(f'==> Finished {((i+1)/len(kg_entities))*100:.4f}% -- {time.perf_counter() - tic:0.2f}s')
 
-query = unidecode('albania')
+query = unidecode('Albania')
 res = es.search(index='csqa_wikidata', doc_type='entities', body={
         'size': 50,
         'query': {
--- scripts/csqa_stats.py
+++ scripts/csqa_stats.py
@@ -9,20 +9,20 @@ ROOT_PATH = Path(os.path.dirname(__file__)).parent.parent
 # %%
 train, val, test = [], [], []
 # read data
-train_files = glob(f'{ROOT_PATH}/data/final/csqa/train/*' + '/*.json')
-for f in train_files:
-    with open(f) as json_file:
-        train.append(json.load(json_file))
+# train_files = glob(f'{ROOT_PATH}/data/final/csqa/train/*' + '/*.json')
+# for f in train_files:
+#     with open(f) as json_file:
+#         train.append(json.load(json_file))
 
 val_files = glob(f'{ROOT_PATH}/data/final/csqa/val/*' + '/*.json')
 for f in val_files:
     with open(f) as json_file:
         val.append(json.load(json_file))
 
-test_files = glob(f'{ROOT_PATH}/data/final/csqa/test/*' + '/*.json')
-for f in test_files:
-    with open(f) as json_file:
-        test.append(json.load(json_file))
+# test_files = glob(f'{ROOT_PATH}/data/final/csqa/test/*' + '/*.json')
+# for f in test_files:
+#     with open(f) as json_file:
+#         test.append(json.load(json_file))
 
 # %%
 ner_spurious = {}
@@ -52,4 +52,20 @@ for qt in total_examples.keys():
         print(f'Percentage of spurious: {(ner_spurious[qt][desc] / total_examples[qt][desc]):.4f}')
 
 
+# %%
+count = 0
+for data in test: # train + val + test:
+    for d in data:
+        if d['speaker'] == 'USER' and 'question-type' in d and d['question-type'] in ['Comparative Reasoning (Count) (All)', 'Quantitative Reasoning (Count) (All)'] and 'description' not in d:
+            count += 1
+print(count)
+# %%
+answer_len = []
+for data in val: # train + val + test:
+    for d in data:
+        if d['speaker'] == 'SYSTEM' and 'all_entities' in d:
+            answer_len.append(len(d['all_entities']))
+print(max(answer_len))
+print(min(answer_len))
+print(sum(answer_len) / len(answer_len))
 # %%
